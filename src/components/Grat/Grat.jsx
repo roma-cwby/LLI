5b40dbb0dc8d7b8d1313a23b163b035a807d40e8
Roma Antoliak <104556918+roma-cwby@users.noreply.github.com>
2023-12-20 15:21:16 +0200

    fix text/img

--- src/components/Grat/Grat.jsx
+++ src/components/Grat/Grat.jsx
@@ -1,16 +1,16 @@
 import { GratSection } from './Grat.styled';
-import img from '../../img/aw.png';
+import img from '../../img/AW.png';
 
 export const Grat = () => {
   return (
     <GratSection>
       <div className="container grat__container">
         <div className="grat_wrap">
-          <h2>Дяка всім, хто були з нами</h2>
+          <h2>Дяка всім друзям, хто були з нами весь цей час</h2>
           <p style={{ color: 'red' }}>nikolo__hard</p>
           <p style={{ color: 'yellow' }}>4elovKEK</p>
           <p style={{ color: 'orange' }}>OleksiiUA92</p>
-          <h2>Безмежна вдячність Альоні</h2>
+          <h2>Безмежна вдячність Альоні. Без тебе нічого б не було</h2>
           <img src={img} alt="logo" />
           <p className="grat_al" style={{ color: '#00ff00' }}>
             adult_woman
